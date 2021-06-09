09e777c11f269860fadc5e02b54faeff97a7b0e4
PedroFirmino <pedrofirmino@hotmail.com>
2021-06-09 14:46:23 -0300

    Implement scene generation from JSON using EJS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let rawdata = fs.readFileSync('./public/json/config.json');
+  let config = JSON.parse(rawdata);
+  res.render('index', config );
 });
 
 module.exports = router;
