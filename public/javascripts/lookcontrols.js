6032b1d62d5618745693d0a46a9219311ac5ea0b
PedroFirmino <pedrofirmino@hotmail.com>
2021-06-15 14:29:42 -0300

    Reverse touch look-controls

--- public/javascripts/lookcontrols.js
+++ public/javascripts/lookcontrols.js
@@ -3,23 +3,20 @@ AFRAME.components[
   "look-controls"
 ].Component.prototype.onTouchMove = function (t) {
   var PI_2 = Math.PI / 2,
-    e,
-    o = this.el.sceneEl.canvas,
-    i = this.yawObject,
-    j = this.pitchObject;
-  this.touchStarted &&
-    this.data.touchEnabled &&
-    ((e =
-      (2 * Math.PI * (t.touches[0].pageX - this.touchStart.x)) /
-      o.clientWidth),
-      (f =
-        (2 * Math.PI * (t.touches[0].pageY - this.touchStart.y)) /
-        o.clientHeight),
-      (j.rotation.x += 0.3 * f),
-      (i.rotation.y += 0.5 * e),
-      (j.rotation.x = Math.max(-PI_2, Math.min(PI_2, j.rotation.x))),
-      (this.touchStart = {
-        x: t.touches[0].pageX,
-        y: t.touches[0].pageY
-      }));
-};
+    deltaX,
+    deltaY,
+    canvas = this.el.sceneEl.canvas,
+    yawObject  = this.yawObject,
+    pitchObject = this.pitchObject,
+    direction;
+
+  this.touchStarted && this.data.touchEnabled && (
+    (direction = this.data.reverseTouchDrag ? 1 : -1),
+    (deltaY = (2 * Math.PI * (t.touches[0].pageX - this.touchStart.x)) / canvas.clientWidth),
+    (deltaX = (2 * Math.PI * (t.touches[0].pageY - this.touchStart.y)) / canvas.clientHeight),
+    (pitchObject.rotation.x -= 0.3 * deltaX * direction),
+    (yawObject.rotation.y -= 0.5 * deltaY * direction),
+    (pitchObject.rotation.x = Math.max(-PI_2, Math.min(PI_2, pitchObject.rotation.x))),
+    (this.touchStart = { x: t.touches[0].pageX, y: t.touches[0].pageY })
+  );
+}
